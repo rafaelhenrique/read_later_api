40ce86231ce6948dd582f514a0411b7a137aee19
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2017-07-04 23:26:31 -0300

    Add insert_links endpoint

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 links = []
+index = 0
 
 
 @app.route('/v1/links', methods=['GET'])
 def list_links():
     return jsonify(links), 200
+
+
+@app.route('/v1/links', methods=['POST'])
+def insert_links():
+    global index
+    global links
+
+    link = request.json['link']
+    description = request.json['description']
+    index += 1
+    data = {
+        'id': index,
+        'link': link,
+        'description': description,
+    }
+    links.append(data)
+    return jsonify(data), 201
 
 
 if __name__ == '__main__':
